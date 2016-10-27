aa6e8faa38f8799c5ae5dde2cac736894060a4d1
Robert Bryant <rabhimself86@gmail.com>
2016-10-27 14:52:20 +0100

    Scaffolded out the favorites page

--- public/app.js
+++ public/app.js
@@ -3,7 +3,8 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.find',
-  'myApp.view2'
+  'myApp.view1',
+  'myApp.favorites'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
