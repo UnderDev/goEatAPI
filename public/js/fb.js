c4e79430380407997f5bcaf32bcf981a87435b80
arnoldout <arnoldout@gmail.com>
2016-10-27 16:31:27 +0100

    View1.js is gone, replaced by fb.js

---
+++ public/js/fb.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.fb', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/fbButton', {
+    templateUrl: 'views/fbLogin.html',
+    controller: 'fbCtrl'
+  });
+}])
+
+.controller('fbCtrl', [function() {
+
+}]);
