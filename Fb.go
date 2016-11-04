67b4b7fb657bb8c24ce7dc1c4020aa6f80e51c66
arnoldout <arnoldout@gmail.com>
2016-11-04 13:31:24 +0000

    live fb push

--- Fb.go
+++ Fb.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	fb "github.com/huandu/facebook"
 )
@@ -13,18 +14,16 @@ func acccessKey(key string) string {
 	})
 	if err == nil {
 		fmt.Println("here is my facebook first name:", res["name"])
-		id := res["id"]
-		resp, goErr := http.Get("https://goeatapi.herokuapp.com/returnFindPerson/" + id)
-		
-		if(goErr==nil)
-		{
-			if resp==undefined
-			{
+		var id string = res["id"].(string)
+		/*resp, goErr := http.Get("https://goeatapi.herokuapp.com/returnFindPerson/" + id)
+		if goErr == nil {
+			fmt.Println(resp)
+			if resp == nil {
 				//create account
-				
+				fmt.Println("sallgood Undefined")
 				//doFunction(res["name"], res["id"])
 			}
-		}
+		}*/
 	}
 	return "hello" //res["first_name"]
 }
