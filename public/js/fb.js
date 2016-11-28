5f3aacbb4741c250e23d114e5c32c2cd814e5310
arnoldout <arnoldout@gmail.com>
2016-11-28 14:18:39 +0000

    Moar logic gating

--- public/js/fb.js
+++ public/js/fb.js
@@ -10,18 +10,18 @@ angular.module('myApp.fb', ['ngRoute'])
 }])
 
 .controller('FbController', ['$scope', function ($scope) {
-    $scope.usrId = false;
+    $scope.usrId = true;
     $scope.checkState = function () {
         
         if((localStorage.getItem("usrId")!=null)&&(localStorage.getItem("usrId")!=undefined))
         {
             console.log("Setting to true :"+$scope.usrId);
-            $scope.usrId = true;
+            $scope.usrId = false;
             console.log("Set to true :"+$scope.usrId);
         }
         else{
             console.log("Setting to false"+$scope.usrId);
-            $scope.usrId = false;
+            $scope.usrId = true;
             console.log("Set to false"+$scope.usrId);
         }
     }
